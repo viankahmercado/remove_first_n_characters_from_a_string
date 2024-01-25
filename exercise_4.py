a7ad64e1a1b6775e4a436cbfd35a4381055c1ebd
viankahmercado <angelviankahmercado@gmail.com>
2024-01-26 03:45:45 +0800

    pseudocode

--- exercise_4.py
+++ exercise_4.py
@@ -5,3 +5,10 @@
 # Exercise 4: 
 # Remove first n characters from a string
 # Write a program to remove characters from a string starting from zero up to n and return a new string.
+
+# create a function to remove the characters
+
+# ask user input
+
+#print result
+
