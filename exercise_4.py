d56e1c5019afa072cca6ac2aa700061f8c859922
viankahmercado <angelviankahmercado@gmail.com>
2024-01-26 03:05:35 +0800

    description

---
+++ exercise_4.py
@@ -0,0 +1,7 @@
+# Assignment 5:
+# Do the exercise 1-15 in https://pynative.com/python-basic-exercise-for-beginners/
+# Try do challenge yourself by not checking the "solution"
+
+# Exercise 4: 
+# Remove first n characters from a string
+# Write a program to remove characters from a string starting from zero up to n and return a new string.
